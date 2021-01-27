99f39bffecdaf07f62e8c73ef63c316f6c7c0705
David shu <a294562476@gmail.com>
2021-01-27 07:49:35 +0000

    rm bug setting.json and add tests file finish atomic.h

--- base/Atomic.h
+++ base/Atomic.h
@@ -1,5 +1,5 @@
 /**
- * @brief gcc buildin atomic function
+ * @brief gcc buildin atomic function, template class for it
  * Copyright (c) 2021, David Shu. All rights reserved.
  * 
  * Use of this source code is governed by a GPL license
@@ -9,6 +9,7 @@
 #ifndef WEB_SERVER_ATOMIC_H
 #define WEB_SERVER_ATOMIC_H
 
+#include <stdint.h>
 #include "base/Noncopyable.h"
 
 namespace web_server{
@@ -20,7 +21,39 @@ class AtomicInteger : Noncopyable {
 public:
     AtomicInteger() : value_(0){}
     T get() {
-        return __atomic_load_n
+        return __atomic_load_n(&value_, __ATOMIC_SEQ_CST);
+    }
+
+    T get_add(T x) {
+        return __atomic_fetch_add(&value_, x, __ATOMIC_SEQ_CST);
+    }
+
+    T add_get(T x) {
+        return __atomic_add_fetch(&value_, x, __ATOMIC_SEQ_CST);
+    }
+
+    T get_set(T new_value) {
+        return __atomic_exchange_n(&value_, new_value, __ATOMIC_SEQ_CST);
+    }
+
+    T increment_get() {
+        return add_get(1);
+    }
+
+    T decrement_get() {
+        return add_get(-1);
+    }
+
+    void add(T x) {
+        get_add(x);
+    }
+
+    void increment() {
+        increment_get();
+    }
+
+    void decrement() {
+        decrement_get();
     }
 
 private:
@@ -29,6 +62,9 @@ private:
 };
 
 } // namespace detail
+
+using AtomicInt32 = detail::AtomicInteger<int32_t>;
+using AtomicInt64 = detail::AtomicInteger<int64_t>;
 
 } // namespace web_server
 
---
+++ base/tests/Atomic_unittest.cc
@@ -0,0 +1,44 @@
+/**
+ * @brief atomic_unittest file
+ * Copyright (c) 2021, David Shu. All rights reserved.
+ * 
+ * Use of this source code is governed by a GPL license
+ * @author David Shu (a294562476@gmail.com)
+ */
+
+#include "base/Atomic.h"
+#include <assert.h>
+
+int main() {
+    {
+        web_server::AtomicInt64 a0;
+        assert(a0.get() == 0);
+        assert(a0.get_add(1) == 0);
+        assert(a0.get() == 1);
+        assert(a0.add_get(2) == 3);
+        assert(a0.get() == 3);
+        assert(a0.increment_get() == 4);
+        assert(a0.get() == 4);
+        a0.increment();
+        assert(a0.get() == 5);
+        assert(a0.add_get(-3) == 2);
+        assert(a0.get_set(100) == 2);
+        assert(a0.get() == 100);
+    }
+
+    {
+        web_server::AtomicInt32 a1;
+        assert(a1.get() == 0);
+        assert(a1.get_add(1) == 0);
+        assert(a1.get() == 1);
+        assert(a1.add_get(2) == 3);
+        assert(a1.get() == 3);
+        assert(a1.increment_get() == 4);
+        assert(a1.get() == 4);
+        a1.increment();
+        assert(a1.get() == 5);
+        assert(a1.add_get(-3) == 2);
+        assert(a1.get_set(100) == 2);
+        assert(a1.get() == 100);
+  }
+}
