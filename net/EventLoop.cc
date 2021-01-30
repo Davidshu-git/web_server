c141b753a4126d4b239bd471215521b6931663d1
David shu <a294562476@gmail.com>
2021-01-30 08:09:34 +0000

    add eventloop.cc

--- net/EventLoop.cc
+++ net/EventLoop.cc
@@ -7,3 +7,131 @@
  */
 
 #include "net/EventLoop.h"
+
+#include <sys/eventfd.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include <vector>
+
+using web_server::net::EventLoop;
+
+namespace  {
+
+__thread EventLoop *t_loop_in_this_thread = 0;
+
+int create_event_fd() {
+    int event_fd = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+    if(event_fd < 0) {
+        abort();
+    }
+    return event_fd;
+}
+
+} // namespace 
+
+namespace web_server {
+
+namespace net {
+
+EventLoop *EventLoop::get_event_loop_of_current_thread() {
+    return t_loop_in_this_thread;
+}
+
+EventLoop::EventLoop() 
+    : looping_(false),
+      quit_(false),
+      event_handleing_(false),
+      calling_pending_functors_(false),
+      iteration_(0),
+      thread_ID_(current_thread::tid()),
+      wakeup_fd_(create_event_fd()),
+      mutex_() {
+    t_loop_in_this_thread = this;
+    // TODO
+    // add wakeup channel
+}
+
+EventLoop::~EventLoop() {
+    // TODO
+    // add channel
+    ::close(wakeup_fd_);
+    t_loop_in_this_thread = NULL;
+}
+
+void EventLoop::loop() {
+    assert(!looping_);
+    assert_in_loop_thread();
+    looping_ = true;
+    quit_ = false;
+
+    while (!quit_) {
+        ++iteration_;
+        event_handleing_ = true;
+        // TODO
+        // add channel for loop
+        event_handleing_ = false;
+        do_pending_functors();
+    }
+
+    looping_ = false;
+}
+
+void EventLoop::quit() {
+    quit_ = true;
+    if (!is_in_loop_thread()) {
+        wakeup();
+    }
+}
+
+void EventLoop::run_in_loop(Functor cb) {
+    if (!is_in_loop_thread()) {
+        cb();
+    } else {
+        queue_in_loop(cb);
+    }
+}
+
+void EventLoop::queue_in_loop(Functor cb) {
+    {
+        MutexLockGuard lock(mutex_);
+        pending_functors_.push_back(cb);
+    }
+    if (!is_in_loop_thread() || calling_pending_functors_) {
+        wakeup();
+    }
+}
+
+// TODO
+// add time function
+// add channel function
+
+void EventLoop::abort_not_in_loop_thread() {
+    printf("EventLoop thread_ID = %d, current thread id = %d\n", thread_ID_, current_thread::tid());
+}
+
+void EventLoop::wakeup() {
+    printf("add socker operation\n");
+}
+
+void EventLoop::handle_read() {
+    printf("add socker operation\n");
+}
+
+void EventLoop::do_pending_functors() {
+    std::vector<Functor> functors;
+    calling_pending_functors_ = true;
+    {
+        MutexLockGuard lock(mutex_);
+        functors.swap(pending_functors_);
+    }
+    for(const auto &functor : functors) {
+        functor();
+    }
+    calling_pending_functors_ = false;
+}
+
+} // namespace net
+
+} // namespace web_server
--- net/EventLoop.h
+++ net/EventLoop.h
@@ -9,37 +9,84 @@
 #ifndef WEB_SERVER_NET_EVENTLOOP_H
 #define WEB_SERVER_NET_EVENTLOOP_H
 
-#include <pthread.h>
+#include <functional>
+#include <atomic>
+#include <vector>
 
 #include "base/Noncopyable.h"
+#include "base/Mutex.h"
+#include "base/CurrentThread.h"
 
 namespace web_server {
 
 namespace net {
 
-/**
- * @brief EventLoop的接口类，不要暴露过多细节
- * 
- */
 class EventLoop : private Noncopyable {
 public:
+    using Functor = std::function<void()>;
+
     EventLoop();
     ~EventLoop();
 
+    int64_t iteration() const {
+        return iteration_;
+    }
+
+    bool is_in_loop_thread() const {
+        return thread_ID_ == current_thread::tid();
+    }
+
+    void assert_in_loop_thread() {
+        if(!is_in_loop_thread()) {
+            abort_not_in_loop_thread();
+        }
+    }
+
+    bool event_handleing() const {
+        return event_handleing_;
+    }
+
+    size_t queue_size() const {
+        MutexLockGuard lock(mutex_);
+        return pending_functors_.size();
+    }
+
     void loop();
-
-    // 断言如果不是在线程中调用则退出
-    void assertInLoopThread();
-
-    // 调用线程是否为I/O线程
-    // @return true/false
-    bool isInLoopThread() const;
+    void quit();
+    // TODO
+    // add poll return time
+    void run_in_loop(Functor cb);
+    void queue_in_loop(Functor cb);
+    // TODO
+    // add timer function
+    void wakeup();
+    // TODO
+    // add channel function
+    // add context function
+    static EventLoop *get_event_loop_of_current_thread();
 
 private:
-    void abortNotInLoopThread();
+    void abort_not_in_loop_thread();
+    void handle_read();
+    void do_pending_functors();
+    // TODO
+    // add print active channel for debug
 
-    bool looping_;                      // 是否在事件循环中
-    const pid_t threadID_;              // EventLoop所属线程ID
+    bool looping_;
+    std::atomic<bool> quit_;
+    bool event_handleing_;
+    bool calling_pending_functors_;
+    int64_t iteration_;
+    const pid_t thread_ID_;
+    // TODO
+    // add timestamp poll return time
+    // add unique_ptr of poller and timequeue
+    int wakeup_fd_;
+    // TODO
+    // add context_
+    // add channel
+    mutable MutexLock mutex_;
+    std::vector<Functor> pending_functors_;
 };
 
 } // namespace net
