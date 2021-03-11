3a4a66bb5728e54a0cceb45bbb5fc9de23dc05e3
David shu <a294562476@gmail.com>
2021-03-11 12:37:46 +0000

    add tcp server

---
+++ net/TcpServer.cc
@@ -0,0 +1,97 @@
+/**
+ * @brief 
+ * Copyright (c) 2021, David Shu. All rights reserved.
+ * 
+ * Use of this source code is governed by a GPL license
+ * @author David Shu (a294562476@gmail.com)
+ */
+
+#include "net/TcpServer.h"
+
+#include <cassert>
+
+#include "base/Logging.h"
+#include "net/EventLoop.h"
+#include "net/Acceptor.h"
+#include "net/EventLoopThreadPool.h"
+
+namespace web_server {
+
+namespace net {
+
+TcpServer::TcpServer(EventLoop *loop, const InetAddress &listen_addr,
+                     const std::string &name, Option option)
+    : loop_(loop),
+      IP_port_(listen_addr.to_IP_port()),
+      name_(name),
+      acceptor_(new Acceptor(loop, listen_addr, option == kReusePort)),
+      thread_pool_(new EventLoopThreadPool(loop, name_)),
+      connection_callback_(default_connection_callback),
+      message_callback_(default_message_callback),
+      next_conn_ID_(1) {
+    acceptor_->set_new_connection_callback(std::bind(&TcpServer::new_connection, this, _1, _2));
+}
+
+TcpServer::~TcpServer() {
+    loop_->assert_in_loop_thread();
+    LOG_TRACE << "TcpServer::~TcpServer [" << name_ << "] destructing";
+
+    for (auto &item : connections_) {
+        TcpConnectionPtr conn(item.second);
+        item.second.reset();
+        conn->get_loop()->run_in_loop(std::bind(&TcpConnection::connection_destroyed, conn));
+    }
+}
+
+void TcpServer::set_thread_num(int num_threads) {
+    assert(num_threads >= 0);
+    thread_pool_->set_thread_num(num_threads);
+}
+
+void TcpServer::start() {
+    if (started_.get_set(1) == 0) {
+        thread_pool_->start(thread_init_callback_);
+        assert(!acceptor_->is_listening());
+        loop_->run_in_loop(std::bind(&Acceptor::listen, get_pointer(acceptor_)));
+    }
+}
+
+void TcpServer::new_connection(int sockfd, const InetAddress &peer_addr) {
+    loop_->assert_in_loop_thread();
+    EventLoop *IO_loop = thread_pool_->get_next_loop();
+    char buf[64];
+    snprintf(buf, sizeof buf, "-%s#%d", IP_port_.c_str(), next_conn_ID_);
+    ++next_conn_ID_;
+    std::string conn_name = name_ + buf;
+
+    LOG_INFO << "TcpServer::newConnection [" << name_
+             << "] - new connection [" << conn_name
+             << "] from " << peer_addr.to_IP_port();
+
+    InetAddress local_addr(InetAddress::get_local_addr(sockfd));
+    TcpConnectionPtr conn(new TcpConnection(IO_loop, conn_name, sockfd, local_addr, peer_addr));
+    connections_[conn_name] = conn;
+    conn->set_connection_callback(connection_callback_);
+    conn->set_message_callback(message_callback_);
+    conn->set_write_complete_callback(write_complete_callback_);
+    conn->set_close_callback(std::bind(&TcpServer::remove_connection, this, _1));
+    IO_loop->run_in_loop(std::bind(&TcpConnection::connection_established, conn));
+}
+
+void TcpServer::remove_connection(const TcpConnectionPtr &conn) {
+    loop_->run_in_loop(std::bind(&TcpServer::remove_connection_in_loop, this, conn));
+}
+
+void TcpServer::remove_connection_in_loop(const TcpConnectionPtr &conn) {
+    loop_->assert_in_loop_thread();
+    LOG_INFO << "TcpServer::removeConnectionInLoop [" << name_
+             << "] - connection " << conn->name();
+    size_t n = connections_.erase(conn->name());
+    assert(n == 1);
+    EventLoop *IO_loop = conn->get_loop();
+    IO_loop->queue_in_loop(std::bind(&TcpConnection::connection_destroyed, conn));
+}
+
+} // namespace net
+
+} // namespace web_server
---
+++ net/TcpServer.h
@@ -0,0 +1,102 @@
+/**
+ * @brief 
+ * Copyright (c) 2021, David Shu. All rights reserved.
+ * 
+ * Use of this source code is governed by a GPL license
+ * @author David Shu (a294562476@gmail.com)
+ */
+
+#ifndef WEB_SERVER_NET_TCPSERVER_H
+#define WEB_SERVER_NET_TCPSERVER_H
+
+#include <map>
+#include <string>
+#include <memory>
+#include <functional>
+#include <cassert>
+
+#include "base/Noncopyable.h"
+#include "base/Atomic.h"
+#include "net/Callbacks.h"
+#include "net/InetAddress.h"
+#include "net/TcpConnection.h"
+
+namespace web_server {
+
+namespace net {
+
+class Acceptor;
+class EventLoop;
+class EventLoopThreadPool;
+
+class TcpServer : private Noncopyable {
+public:
+    using ThreadInitCallback = std::function<void(EventLoop *)>;
+
+    enum Option { kNoReusePort, kReusePort };
+
+    TcpServer(EventLoop *loop, const InetAddress &listen_addr,
+              const std::string &name, Option option = kNoReusePort);
+    ~TcpServer();
+
+    const std::string &IP_port() const {
+        return IP_port_;
+    }
+
+    const std::string &name() const {
+        return name_;
+    }
+
+    EventLoop *get_loop() const {
+        return loop_;
+    }
+
+    void set_thread_num(int num_threads);
+    void set_thread_init_callback(const ThreadInitCallback &cb) {
+        thread_init_callback_ = cb;
+    }
+
+    std::shared_ptr<EventLoopThreadPool> thread_pool() {
+        return thread_pool_;
+    }
+    
+    void start();
+
+    void set_connection_callback(const ConnectionCallback &cb) {
+        connection_callback_ = cb;
+    }
+
+    void set_message_callback(const MessageCallback &cb) {
+        message_callback_ = cb;
+    }
+
+    void set_write_complete_callback(const WriteCompleteCallback &cb) {
+        write_complete_callback_ = cb;
+    }
+    
+private:
+    using ConnectionMap = std::map<std::string, TcpConnectionPtr>;
+
+    EventLoop *loop_;
+    const std::string IP_port_;
+    const std::string name_;
+    std::unique_ptr<Acceptor> acceptor_;
+    std::shared_ptr<EventLoopThreadPool> thread_pool_;
+    ConnectionCallback connection_callback_;
+    MessageCallback message_callback_;
+    WriteCompleteCallback write_complete_callback_;
+    ThreadInitCallback thread_init_callback_;
+    AtomicInt32 started_;
+    int next_conn_ID_;
+    ConnectionMap connections_;
+
+    void new_connection(int sockfd, const InetAddress &peer_addr);
+    void remove_connection(const TcpConnectionPtr &conn);
+    void remove_connection_in_loop(const TcpConnectionPtr &conn);
+};
+
+} // namespace net
+
+} // namespace web_server
+
+#endif // WEB_SERVER_NET_TCPSERVER_H
